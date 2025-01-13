cad4e9864a787d657915f912e8d14b6fb9a078cb
Shrutikuratkar <shrutikuratkar16@gmail.com>
2025-01-13 16:57:16 +0530

    v2-autocompleteTask with filtering entity

--- src/MaterialModule.ts
+++ src/MaterialModule.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
 @NgModule({
     exports:[
         MatInputModule,
-        MatSelectModule
+        MatSelectModule,
+        MatAutocompleteModule
     ]
 })
 
